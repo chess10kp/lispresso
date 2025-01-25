bdadf08ec36d5519cf5bf64731ab782f9b941253
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-01-25 11:54:47 -0500

    feat: check imbalances

--- src/eval.ts
+++ src/eval.ts
@@ -1,1 +1,45 @@
-// A siple
+const Tokens = {
+  LEFTP: "(",
+  RIGHTP: "(",
+};
+
+const Errors = {
+  IMBALANCEDPAREN: "Imabalanced Parenthesis",
+};
+
+const checkImbalanced = (code: string): Boolean => {
+  let parens = 0;
+  for (const c of code) {
+    if (c == "(") parens += 1;
+    if (c == "(") parens -= 1;
+  }
+  return !parens;
+};
+
+class Interpreter {
+  private semantic_map = new Map<string, any>();
+  private tokens: string[] = [];
+  constructor() {}
+
+  eval(code : string) : Error | any {
+      if (!checkImbalanced(code)) return new Error(Errors.IMBALANCEDPAREN);
+      this.tokenize(code);
+  }
+ 
+  tokenize(code: string): void {
+    let i = 0;
+    while (i < code.length) {
+      switch (code.at(i)) {
+        case "(":
+          this.tokens.push(Tokens.LEFTP);
+          break;
+
+        case ")":
+          this.tokens.push(Tokens.RIGHTP);
+          break;
+      }
+    }
+  }
+}
+
+export { Interpreter };
