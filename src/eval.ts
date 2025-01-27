76d5690f71e9eeb473a6383c79c602fa57fbedad
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-01-26 19:37:11 -0500

    feat(eval): parse simple addition

--- src/eval.ts
+++ src/eval.ts
@@ -1,3 +1,5 @@
+import { assert } from "node:console";
+
 const TokenType = {
   LEFTP: "(",
   RIGHTP: ")",
@@ -18,8 +20,9 @@ const TokenType = {
   IDENTIFIER: "identifier",
   UNDEFINED: undefined,
   DO: "do",
+  DEF: "def",
   ELSE: "else",
-  LAMBDA: "lambda",
+  FN: "fn",
   LET: "let",
   IF: "if",
   NUMBER: "number",
@@ -32,19 +35,23 @@ const keyWords = new Set([
   "if",
   "else",
   "not",
-  "lambda",
+  "fn",
   "let",
+  "def",
 ]);
 
 const Errors = {
   IMBALANCEDPAREN: "Imabalanced Parenthesis",
   MISMATCHEDTYPE: "Mismatched Type",
   MISMATCHEDPAREN: "Mismatched Parenthesis",
+  SYNTAXERROR: "Syntax Error",
   UNKNOWN: "Unknown Error",
+  UNKNOWNVARIABLE: "Unknown Variable",
+  NAN: "NAN",
 };
 
 type Token = {
-  type: string | undefined;
+  type: string;
   value: string;
 };
 
@@ -66,17 +73,17 @@ class Interpreter {
 
   constructor() {}
 
-  eval(code: string): Error | any {
-    if (!checkImbalanced(code)) return new Error(Errors.IMBALANCEDPAREN);
+  eval(code: string): (Error | Token)[] {
+    if (!checkImbalanced(code)) return [new Error(Errors.IMBALANCEDPAREN)];
     this.tokenize(code);
     this.nest();
-    return this;
+    console.dir(this.nestedExpressions, { depth: null });
+    return this.execute();
   }
 
-  semantic_check(): Interpreter {
-    execute(this.nestedExpressions, this.table);
-
-    return this;
+  execute(): (Token | Error)[] {
+    const value = execute(this.nestedExpressions, this.table);
+    return value;
   }
 
   printTokens(): Interpreter {
@@ -94,44 +101,6 @@ class Interpreter {
     return this.nestedExpressions;
   }
 
-  getIdent(ident: string): any | undefined {
-    let tab = this.table[this.table.length - 1];
-    while (tab) {
-      if (tab.has(ident)) {
-        return tab.get(ident);
-      }
-      tab = this.table[this.table.length - 1];
-    }
-    return undefined;
-  }
-
-  setIdent(ident: string, value: any): boolean {
-    let tab = this.table[this.table.length - 1];
-    if (tab.has(ident)) {
-      tab.set(ident, value);
-      return true;
-    }
-    return false;
-  }
-
-  initIdent(ident: string, value: any): boolean {
-    let tab = this.table[this.table.length - 1];
-    if (this.getIdent(ident) == undefined) {
-      return false;
-    }
-    tab.set(ident, value);
-    return true;
-  }
-
-  enterScope(): void {
-    this.table.push(new Map());
-  }
-
-  exitScope(): void {
-    this.table.pop();
-  }
-  
- 
   tokenize(code: string): void {
     let i = 0;
     while (i < code.length) {
@@ -204,7 +173,7 @@ class Interpreter {
               const keyword =
                 // @ts-ignore
                 TokenType[ident] ||
-                (!isNaN(parseInt(ident))
+                (isNaN(parseInt(ident))
                   ? TokenType.IDENTIFIER
                   : TokenType.NUMBER);
 
@@ -261,13 +230,181 @@ const nest = (tokens: Token[]): Token[][] => {
 
 type Symtable = Map<String, any>;
 
-const execute = (tokens: Token[][], symtables: Symtable[]) => {};
+const executeAux = (
+  block: (Token | Token[])[] | Token | Token[],
+  symtables: Symtable[],
+): Token | Error => {
+  if (block instanceof Array && !(block[0] instanceof Array)) {
+    if (block[0].type == TokenType.DO) {
+      const [ele, ...rest] = block;
+      for (const block of rest) {
+        executeAux(block, symtables);
+      }
+    } else if (block[0].type == TokenType.DEF) {
+      const [_, ident, value] = block;
+      if (ident instanceof Array) {
+        return new Error(Errors.SYNTAXERROR);
+      }
+
+      if (symtables.length == 0) {
+        symtables.push(new Map());
+      }
+      symtables[symtables.length - 1].set(
+        ident.value,
+        executeAux(value, symtables),
+      );
+    } else if (block[0].type == TokenType.FN) {
+      const [_, ident, ...body] = block;
+      if (ident instanceof Array) {
+        return new Error(Errors.SYNTAXERROR);
+      }
+      if (symtables.length == 0) {
+        symtables.push(new Map());
+      }
+      const functionBody = executeAux(body, symtables);
+      symtables[symtables.length - 1].set(ident.value, functionBody);
+    } else if (block[0].type == TokenType.IDENTIFIER) {
+      const [ident, rest] = block;
+      const value = getIdent(symtables, ident.value);
+      if (value === undefined) {
+        return new Error(Errors.UNKNOWNVARIABLE);
+      }
+      console.log(value);
+      return value;
+    } else if (block[0].type == TokenType.NUMBER) {
+      const [ident, res] = block;
+      const value = parseInt(ident.value);
+      if (isNaN(value)) {
+        return new Error(Errors.NAN);
+      }
+      return block[0];
+    } else if (block[0].type == TokenType.ADD) {
+      const [_, ...rest] = block;
+      const res = rest.reduce((acc, curr) => {
+        if (curr instanceof Array) {
+          // @ts-ignore
+          return acc.value + parseInt(executeAux(curr, symtables).value);
+        } else {
+          if (acc instanceof Array || curr instanceof Array) {
+            return new Error(Errors.UNKNOWN);
+          }
+          return {
+            type: TokenType.NUMBER,
+            value: parseInt(acc.value) + parseInt(curr.value),
+          };
+        }
+      });
+      if (res instanceof Array) {
+        return new Error(Errors.UNKNOWN);
+      }
+      return res;
+    }
+  }
+  return { type: "", value: "" };
+};
+
+const getIdent = (table: Symtable[], ident: string): any | undefined => {
+  if (table.length == 0) {
+    return undefined;
+  }
+  let i = table.length - 1;
+  let tab = table[i];
+  while (tab) {
+    if (tab.has(ident)) {
+      return tab.get(ident);
+    }
+    --i;
+    tab = table[i];
+  }
+  return undefined;
+};
+
+const setIdent = (table: Symtable[], ident: string, value: any) => {
+  if (table.length == 0) {
+    return undefined;
+  }
+  let tab = table[table.length - 1];
+  if (tab.has(ident)) {
+    tab.set(ident, value);
+    return true;
+  }
+  return false;
+};
+
+const initIdent = (table: Symtable[], ident: string, value: any): boolean => {
+  let tab = table[table.length - 1];
+  if (getIdent(table, ident) == undefined) {
+    return false;
+  }
+  tab.set(ident, value);
+  return true;
+};
+
+function enterScope(table: Symtable[]): void {
+  table.push(new Map());
+}
+
+function exitScope(table: Symtable[]): void {
+  table.pop();
+}
+
+const execute = (
+  tokens: (Token | Token[])[],
+  symtables: Symtable[],
+): (Token | Error)[] => {
+  const acc = [];
+  const firstBlock = tokens;
+
+  if (firstBlock == undefined) {
+    return [new Error(Errors.UNKNOWN)];
+  }
+
+  if (firstBlock[0] instanceof Array) {
+    return [new Error(Errors.SYNTAXERROR)];
+  }
+
+  if (firstBlock[0].type == TokenType.DO) {
+    const [ele, ...rest] = firstBlock;
+    for (const block of rest) {
+      acc.push(executeAux(block, symtables));
+    }
+  } else if (firstBlock[0].type == TokenType.DEF) {
+    const [_, ident, value] = firstBlock;
+    if (ident instanceof Array) {
+      return [new Error(Errors.SYNTAXERROR)];
+    }
+
+    if (symtables.length == 0) {
+      symtables.push(new Map());
+    }
+
+    symtables[symtables.length - 1].set(
+      ident.value,
+      executeAux(value, symtables),
+    );
+  } else if (firstBlock[0].type == TokenType.FN) {
+    const [_, ident, ...body] = firstBlock;
+    if (symtables.length == 0) {
+      symtables.push(new Map());
+    }
+
+    if (ident instanceof Array) {
+      return [new Error(Errors.SYNTAXERROR)];
+    }
+
+    symtables[symtables.length - 1].set(
+      ident.value,
+      executeAux(body, symtables),
+    );
+  }
+  return acc;
+};
 
 const i = new Interpreter();
-i.eval("(+ 1 (- 2 (* 3 (/ 1 a))))");
+i.eval("(do (+ 1 1))");
 
 const j = new Interpreter();
-j.eval("(lambda (+ a 1) (+ a 2))");
-console.dir(j.getNested());
+const a = j.eval("(do (+ 1 1))");
+console.dir(a, { depth: null });
 
 export { Interpreter, Errors, TokenType as Tokens };
