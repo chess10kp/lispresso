63e154e3990fe16ff575012fa3bcb83bb707de6d
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-01-27 22:27:49 -0500

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/lispresso",
+  output: "export",
+  reactStrictMode: true, 
 };
 
-export default nextConfig;
+export default nextConfig
